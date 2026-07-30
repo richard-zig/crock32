37b08a98b2a802045232648a5d66de720433d4e0
Richard Lehane <richard@itforarchivists.com>
2026-07-30 12:54:54 +1000

    make errors more specific

--- src/root.zig
+++ src/root.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 
-pub const Crock32Error = error{
+pub const DecodeError = error{
     UnexpectedChar,
     OverflowsU64,
-    BufferOverflow,
     TooShort,
     UnexpectedCheckChar,
     CheckFailed,
+};
+
+pub const EncodeError = error{
+    BufferOverflow,
 };
 
 pub const max_buf_len = 14; // includes check
@@ -41,14 +44,14 @@ fn decodeChar(b: u8) u8 {
 
 // decode performs case insenstive decoding of a crock32 encoded string
 // use for strings encoded by encode() or encodeUpper()
-pub fn decode(str: []const u8) Crock32Error!u64 {
+pub fn decode(str: []const u8) DecodeError!u64 {
     const cutoff: u64 = ((1 << 64) - 1) / 32 + 1;
     var n: u64 = 0;
     for (str) |char| {
         const c = decodeChar(char);
         if (c == 254) continue;
-        if (c >= 32) return Crock32Error.UnexpectedChar;
-        if (n >= cutoff) return Crock32Error.OverflowsU64;
+        if (c >= 32) return error.UnexpectedChar;
+        if (n >= cutoff) return error.OverflowsU64;
         n = n * 32 + c;
     }
     return n;
@@ -56,21 +59,21 @@ pub fn decode(str: []const u8) Crock32Error!u64 {
 
 // decodeCheck performs case insensitive decoding of crock32 encoded strings
 // with a check character (i.e. created with encodeCheck() or encodeUpperCheck())
-pub fn decodeCheck(str: []const u8) Crock32Error!u64 {
-    if (str.len < 2) return Crock32Error.TooShort;
+pub fn decodeCheck(str: []const u8) DecodeError!u64 {
+    if (str.len < 2) return error.TooShort;
     const check = decodeChar(str[str.len - 1]);
-    if (check > 36) return Crock32Error.UnexpectedCheckChar;
+    if (check > 36) return error.UnexpectedCheckChar;
     const val = try decode(str[0 .. str.len - 1]);
-    if (val % 37 != check) return Crock32Error.CheckFailed;
+    if (val % 37 != check) return error.CheckFailed;
     return val;
 }
 
-fn enc(d: []const u8, buf: []u8, n: u64) Crock32Error![]const u8 {
+fn enc(d: []const u8, buf: []u8, n: u64) EncodeError![]const u8 {
     var i = buf.len;
-    if (i == 0) return Crock32Error.BufferOverflow;
+    if (i == 0) return error.BufferOverflow;
     var nn = n;
     while (nn >= 32) {
-        if (i > 1) i -= 1 else return Crock32Error.BufferOverflow;
+        if (i > 1) i -= 1 else return error.BufferOverflow;
         buf[i] = d[nn % 32];
         nn /= 32;
     } else i -= 1;
@@ -81,26 +84,26 @@ fn enc(d: []const u8, buf: []u8, n: u64) Crock32Error![]const u8 {
 // encode n with Crockford's base32 encoding scheme in lower case
 // the buffer should be long enough to hold the result, use
 // max_buf_len if unsure
-pub fn encode(buf: []u8, n: u64) Crock32Error![]const u8 {
+pub fn encode(buf: []u8, n: u64) EncodeError![]const u8 {
     return enc(digits, buf, n);
 }
 
 // encodeCheck encodes n and adds check character to the result
-pub fn encodeCheck(buf: []u8, n: u64) Crock32Error![]const u8 {
-    if (buf.len < 2) return Crock32Error.BufferOverflow;
+pub fn encodeCheck(buf: []u8, n: u64) EncodeError![]const u8 {
+    if (buf.len < 2) return error.BufferOverflow;
     const str = try encode(buf[0 .. buf.len - 1], n);
     buf[buf.len - 1] = digits[n % 37];
     return buf[buf.len - str.len - 1 ..];
 }
 
 // encode upper uses upper case characters for the encoding
-pub fn encodeUpper(buf: []u8, n: u64) Crock32Error![]const u8 {
+pub fn encodeUpper(buf: []u8, n: u64) EncodeError![]const u8 {
     return enc(digitsUpper, buf, n);
 }
 
 // encodeUpperCheck uses upper case characters and adds a check character
-pub fn encodeUpperCheck(buf: []u8, n: u64) Crock32Error![]const u8 {
-    if (buf.len < 2) return Crock32Error.BufferOverflow;
+pub fn encodeUpperCheck(buf: []u8, n: u64) EncodeError![]const u8 {
+    if (buf.len < 2) return error.BufferOverflow;
     const str = try encodeUpper(buf[0 .. buf.len - 1], n);
     buf[buf.len - 1] = digitsUpper[n % 37];
     return buf[buf.len - str.len - 1 ..];
@@ -131,7 +134,7 @@ test "decodeCheck" {
 }
 
 test "decodeCheck bad check" {
-    try std.testing.expect(decodeCheck("yolo=") == Crock32Error.CheckFailed);
+    try std.testing.expect(decodeCheck("yolo=") == error.CheckFailed);
 }
 
 test "encodeCheck" {
